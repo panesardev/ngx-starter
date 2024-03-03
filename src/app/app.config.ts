9361d626d94807a328585a25bb33a6266ff7592a
panesar.pb08 <panesar.pb08@gmail.com>
2024-03-03 16:12:01 +0000

    refactor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,8 @@
-import { ApplicationConfig } from '@angular/core';
-import { PreloadAllModules, provideRouter, withInMemoryScrolling, withPreloading } from '@angular/router';
-
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { ApplicationConfig } from '@angular/core';
+import { provideClientHydration } from '@angular/platform-browser';
+import { PreloadAllModules, provideRouter, withInMemoryScrolling, withPreloading } from '@angular/router';
+import { routes } from './app.routes';
 import { provideFirebase } from './providers/firebase.provider';
 
 export const appConfig: ApplicationConfig = {
