42e7b321312166058512b37e0e52d55d9750c095
Sukhpreet Singh <panesar.pb08@gmail.com>
2024-03-20 15:51:57 -0400

    refactor: AuthService

--- src/app/utilities/auth.guard.ts
+++ src/app/utilities/auth.guard.ts
@@ -1,12 +1,13 @@
 import { inject } from "@angular/core";
 import { CanActivateFn, Router } from "@angular/router";
 import { AuthService } from "../services/auth.service";
-import { map, tap } from "rxjs";
+import { map, take, tap } from "rxjs";
 
 export const authGuard: CanActivateFn = () => {
   const auth = inject(AuthService);
   const router = inject(Router);
   return auth.user$.pipe(
+    take(1),
     map(user => !!user),
     tap(exists => !exists && router.navigateByUrl('/login')),
   );
